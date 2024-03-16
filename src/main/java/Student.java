0937d02082f5ad634928cc54a1a2c68f251f4544
Amir Hossein Ziaeifar <amirsalam2004@gmail.com>
2024-03-16 14:08:16 +0330

    creating Student classes

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Student extends Account{
+    ArrayList<String> courses;
+    ArrayList<String> teachers;
+    public Student(){
+        courses=new ArrayList<>();
+        teachers=new ArrayList<>();
+    }
+    public ArrayList<String> getAllowedCourse(ArrayList<String> courses){
+        ArrayList<String> allowedCoursesList=new ArrayList<>();
+        for(int i=0;i<courses.size();i++){
+            if(!this.courses.contains(courses.get(i)))
+                allowedCoursesList.add(courses.get(i));
+        }
+        return allowedCoursesList;
+    }
+    public void addCourse(String courseName,String teacherName){
+        this.courses.add(courseName);
+        this.teachers.add(teacherName);
+    }
+    public ArrayList<String> getTeachers(){return this.teachers;}
+    public ArrayList<String> getCourses(){return this.courses;}
+
 }
